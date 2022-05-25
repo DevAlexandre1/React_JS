b263bf9f1b2c34e656094c918bd910af0269741f
Alexandre Henrique <alexandre_eaw@hotmail.com>
2022-05-25 11:11:33 -0300

    Update App.js

--- treinamento/src/App.js
+++ treinamento/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Painel from './components/Painel'
+import {useState} from 'react'
 // import Tarefa from "./components/Task"
 // import Somar from "./components/Somar"
 // import Menu from "./components/Menu"
@@ -7,11 +8,17 @@ import Painel from './components/Painel'
 // import Rodape from "./components/Rodape"
 
 function App() {
-
+const [btnMenu, setBtnMenu] = useState(false);
+const btnOnClick =()=>setBtnMenu(!btnMenu)
+console.log(btnMenu)
   
+
   return (
     <div className="App">
-      <Painel/>
+      {/* <Painel/> */}
+      <h1>Testando</h1>
+      <button onClick={btnOnClick}>Clique aqui</button>
+
     </div>
   );
 }
