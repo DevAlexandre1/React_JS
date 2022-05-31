9b1322dff35d83d3c9d244590126605c3ddf7bfb
Alexandre Henrique <83831769+DevAlexandre1@users.noreply.github.com>
2022-05-30 21:40:56 -0300

    Update Task.js

--- treinamento/src/components/Task.js
+++ treinamento/src/components/Task.js
@@ -2,34 +2,34 @@ import { useState } from "react"
 import styles from './Task.module.css'
 
 const Task = (props) => {
-    const {tarefas} = props;
+  const {tarefas} = props;
+  
+  //Botoes
+  const [btnIniciar, setBtnIniciar] = useState(true);
+  const [btnParar, setBtnParar] = useState(true);
+  const [btnGravar, setBtnGravar] = useState(true);
+  
+  //Dados tarefas
+    const [tarefa0,setTarefa0] = useState()
+    const [tarefa1,setTarefa1] = useState()
+    const [tarefa2,setTarefa2] = useState()
+    const [tarefa3,setTarefa3] = useState()
+    const [tarefa4,setTarefa4] = useState()
+    const [tarefa5,setTarefa5] = useState()
     
-    //Botoes
-    const [btnIniciar, setBtnIniciar] = useState(true);
-    const [btnParar, setBtnParar] = useState(true);
-    const [btnGravar, setBtnGravar] = useState(true);
-    
-    //Dados tarefas
-    const [tarefa0,setTarefa0] = useState("")
-    const [tarefa1,setTarefa1] = useState("")
-    const [tarefa2,setTarefa2] = useState("")
-    const [tarefa3,setTarefa3] = useState("")
-    const [tarefa4,setTarefa4] = useState("")
-    const [tarefa5,setTarefa5] = useState("")
-    
-    const [empresa0, setEmpresa0] = useState("")
-    const [empresa1, setEmpresa1] = useState("")
-    const [empresa2, setEmpresa2] = useState("")
-    const [empresa3, setEmpresa3] = useState("")
-    const [empresa4, setEmpresa4] = useState("")
-    const [empresa5, setEmpresa5] = useState("")
+    const [empresa0, setEmpresa0] = useState()
+    const [empresa1, setEmpresa1] = useState()
+    const [empresa2, setEmpresa2] = useState()
+    const [empresa3, setEmpresa3] = useState()
+    const [empresa4, setEmpresa4] = useState()
+    const [empresa5, setEmpresa5] = useState()
 
-    const [comentario0, setComentario0] = useState("")
-    const [comentario1, setComentario1] = useState("")
-    const [comentario2, setComentario2] = useState("")
-    const [comentario3, setComentario3] = useState("")
-    const [comentario4, setComentario4] = useState("")
-    const [comentario5, setComentario5] = useState("")
+    const [comentario0, setComentario0] = useState()
+    const [comentario1, setComentario1] = useState()
+    const [comentario2, setComentario2] = useState()
+    const [comentario3, setComentario3] = useState()
+    const [comentario4, setComentario4] = useState()
+    const [comentario5, setComentario5] = useState()
     
     //filhos de tarefa_container
     
@@ -54,46 +54,64 @@ const Task = (props) => {
        
     //     console.log("Tarefa finalizada!")
     // }
+   
 
     const handleBtnIniciar =()=>{
-        setBtnIniciar(!btnIniciar)
-        console.log("Iniciou?",btnIniciar)
-        console.log("Parou?",btnParar)
-        console.log("Gravou?",btnGravar)
+        
+      const select = document.querySelector("#tarefa")
+      let valor = select.options[select.selectedIndex].text
+      setTarefa0(valor)
+      console.log(tarefa0)
+      
+      // setBtnIniciar(!btnIniciar)
+        // console.log("Iniciou?",btnIniciar)
+        // console.log("Parou?",btnParar)
+        // console.log("Gravou?",btnGravar)
 
-
+        
         // const handleTarefa =()=>{
         //     // e.presentDefault();
             
-        const tarefasAll = document.querySelector("#tarefa")
+        // const tarefasAll = document.querySelector("#tarefa")
         
-        let tarefa00 = tarefasAll[0].value
-        let tarefa01 = tarefasAll[1].value
-        let tarefa02 = tarefasAll[2].value
-        let tarefa03 = tarefasAll[3].value
-        let tarefa04 = tarefasAll[4].value
-        let tarefa05 = tarefasAll[5].value
+
+  
+        // const retornoValTask = tarefa.forEach(function(el,i){
+        //    let value = tarefa[i]   
+        //    let select = value.options[value.selectedIndex].text 
+        //    setTarefa0(select) 
+        //    console.log(tarefa0)
+        // })
+
+        // function valtask =()
+
+        // let tarefa00 = tarefasAll[0].value
+        // let tarefa01 = tarefasAll[1].value
+        // let tarefa02 = tarefasAll[2].value
+        // let tarefa03 = tarefasAll[3].value
+        // let tarefa04 = tarefasAll[4].value
+        // let tarefa05 = tarefasAll[5].value
     
-            const tarefasIn0 = tarefasAll[0].selectedIndex
-            const tarefasIn1 = tarefasAll[1].selectedIndex
-            const tarefasIn2 = tarefasAll[2].selectedIndex
-            const tarefasIn3 = tarefasAll[3].selectedIndex
-            const tarefasIn4 = tarefasAll[4].selectedIndex
-            const tarefasIn5 = tarefasAll[5].selectedIndex
+        //     const tarefasIn0 = tarefasAll[0].selectedIndex
+        //     const tarefasIn1 = tarefasAll[1].selectedIndex
+        //     const tarefasIn2 = tarefasAll[2].selectedIndex
+        //     const tarefasIn3 = tarefasAll[3].selectedIndex
+        //     const tarefasIn4 = tarefasAll[4].selectedIndex
+        //     const tarefasIn5 = tarefasAll[5].selectedIndex
             
-            setTarefa0(tarefasIn0)
-            setTarefa1(tarefasIn1)
-            setTarefa2(tarefasIn2)
-            setTarefa3(tarefasIn3)
-            setTarefa4(tarefasIn4)
-            setTarefa5(tarefasIn5)
+            // setTarefa0(tarefasIn0)
+            // setTarefa1(tarefasIn1)
+            // setTarefa2(tarefasIn2)
+            // setTarefa3(tarefasIn3)
+            // setTarefa4(tarefasIn4)
+            // setTarefa5(tarefasIn5)
             
-            console.log(tarefa0)
-            console.log(tarefa1)
-            console.log(tarefa2)
-            console.log(tarefa3)
-            console.log(tarefa4)
-            console.log(tarefa5)
+            // console.log(tarefa0)
+            // console.log(tarefa1)
+            // console.log(tarefa2)
+            // console.log(tarefa3)
+            // console.log(tarefa4)
+            // console.log(tarefa5)
      }
     
 
