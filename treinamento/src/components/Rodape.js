d129459f9b8593995debbd260777c336a578475c
Alexandre Henrique <alexandre_eaw@hotmail.com>
2022-06-02 12:07:53 -0300

    Criando os botoes stop e start All

--- treinamento/src/components/Rodape.js
+++ treinamento/src/components/Rodape.js
@@ -16,8 +16,15 @@ const addTask=()=>{
 
   return (
     <div  className={styles.rodape}>
+      <div className={styles.btn_add_div}>
           <button className={styles.btn_addTask} onClick={addTask} type="submit">Add task</button>     
+          <button className={styles.btn_addTask} onClick={addTask} type="submit">Start All</button>    
+          <button className={styles.btn_addTask} onClick={addTask} type="submit">Stop All</button>    
+      </div>
+      <div>
         {<Task tarefas={task}/> }
+      </div>
+
     </div>
   )
 }
